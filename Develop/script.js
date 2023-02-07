6c7285d2859b0a3ba408c464fb0427008c11aef9
Tifannie Truman <nietru143@gmail.com>
2023-02-07 14:01:45 -0500

    iterated through my timeBoxes array to fill the workday hour boxes

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,5 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
+// done with $(function () {    at the start of the code.
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 // Resource for possibly changing color after hour changes current past or future:  https://jqueryui.com/addClass/
@@ -6,9 +7,11 @@
 $(function () {
   var currentDay = $("#currentDay");
   // Displays the current day and current time in the header section
-  $("#currentDay").text(dayjs().format("[Today is]  MMM DD, YYYY [at] h:mm a"));
+  $("#currentDay").text(
+    dayjs().format("[Today is]  MMM DD, YYYY [at hour:] h" + ":00")
+  );
   // array for hour blocks on left of text area
-  let timeBoxes = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
+  let timeBoxes = [9, 10, 11, 12, 13, 14, 15, 16, 17];
   var container = $(".container-lg");
   // dynamically create the content inside of schedule container:
   for (i = 0; i < timeBoxes.length; i++) {
@@ -18,7 +21,8 @@ $(function () {
     var button = $("<button>");
     var saveIcon = $("<i>");
     //add the classes and styling to the elements:
-    timeBox.addClass("col-2 col-md-1 #hour text-center py-3");
+    timeBox.addClass("col-2 col-md-1 hour row time-block text-center py-3");
+    // .text(timeBoxes[i]);
     textArea
       .addClass("col-8 col-md-10 #textArea description rows=3")
       .attr("rows", "3");
@@ -29,10 +33,15 @@ $(function () {
 
     //apend elements to the dom:
     container.append(row);
+    timeBox.append(timeBoxes[i] + ":00"); // displays time in the timeboxes from the array.
     row.append(timeBox, textArea, button);
     button.append(saveIcon);
 
     // save button functionality
+    $(row).on("click", "button.saveBtn", function () {
+      var button = $(this);
+      console.log(button);
+    });
     // $(".button").on("click", function () {
     //   let userInput = $(this).siblings("#textArea").val();
     //   let timeBox = $(this).siblings("#textArea").attr("id");
@@ -40,10 +49,10 @@ $(function () {
     //   localStorage.setItem(timeBox, userInput);
     // });
 
-    //Loop pulls from local storage.
-    // for (i = 9; i <= 17; i++) {
-    //   $("#hour-" + i).text(localStorage.getItem("hour-" + i));
-    // }
+    //   // Loop pulls from local storage.
+    //   for (i = 9; i <= 17; i++) {
+    //     $("#hour" + i).text(localStorage.getItem("#hour" + i));
+    //   }
   }
 });
 
