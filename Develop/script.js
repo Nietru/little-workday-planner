79156a7beb20d18fa69cd675b226a295bf21a398
Tifannie Truman <nietru143@gmail.com>
2023-02-07 14:52:28 -0500

    got the color change function somewhat working and added button functionality that is not working yet

--- Develop/script.js
+++ Develop/script.js
@@ -8,7 +8,7 @@ $(function () {
   var currentDay = $("#currentDay");
   // Displays the current day and current time in the header section
   $("#currentDay").text(
-    dayjs().format("[Today is]  MMM DD, YYYY [at hour:] h" + ":00")
+    dayjs().format("[Today is]  MMM DD, YYYY [at hour:] H" + ":00")
   );
   // array for hour blocks on left of text area
   let timeBoxes = [9, 10, 11, 12, 13, 14, 15, 16, 17];
@@ -27,7 +27,9 @@ $(function () {
       .addClass("col-8 col-md-10 #textArea description rows=3")
       .attr("rows", "3");
 
-    button.addClass("col-2 col-md-1 btn .saveBtn").attr("aria-label", "save");
+    button
+      .addClass("col-2 col-md-1 btn .saveBtn")
+      .attr("aria-label", "save", "data-index", i);
 
     saveIcon.addClass("fas fa-save").attr("aria-hidden", "true");
 
@@ -37,23 +39,20 @@ $(function () {
     row.append(timeBox, textArea, button);
     button.append(saveIcon);
 
+    //   // add class depending on time of the workday
+    $(function colorChange() {
+      if (timeBox[i] < dayjs(currentDay)) {
+        row.addClass("future");
+      } else if (timeBox[i] > dayjs(currentDay)) {
+        row.addClass("past");
+      } else timeBox;
+      row.addClass("present");
+    });
+
     // save button functionality
     $(row).on("click", "button.saveBtn", function () {
       var button = $(this);
-      console.log(button);
+      console.log(typeof button.attr("data-index"));
     });
-    // $(".button").on("click", function () {
-    //   let userInput = $(this).siblings("#textArea").val();
-    //   let timeBox = $(this).siblings("#textArea").attr("id");
-
-    //   localStorage.setItem(timeBox, userInput);
-    // });
-
-    //   // Loop pulls from local storage.
-    //   for (i = 9; i <= 17; i++) {
-    //     $("#hour" + i).text(localStorage.getItem("#hour" + i));
-    //   }
   }
 });
-
-/** APP INIT */
